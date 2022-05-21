f31af426a8d480a8c1a61a7f308c7246d46642d1
TheShadowRaptor <90637945+TheShadowRaptor@users.noreply.github.com>
2022-05-21 02:32:06 -0300

    [ADDED] First Person Camera/MouseLook Script

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private Transform playerBody;
+
+    private float xRotation = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Lock Mouse
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Initilizing MouseLook
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Look Up/Down
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -45f, 45f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // Look Left/Right
+        playerBody.Rotate(Vector3.up * mouseX);
+
     }
 }
