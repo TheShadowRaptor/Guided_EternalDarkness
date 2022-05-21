971fc8669e37e50ef5f8dd0fb60b9de6fd3286e0
TheShadowRaptor <90637945+TheShadowRaptor@users.noreply.github.com>
2022-05-21 03:34:21 -0300

    [UPDATED] PlayeMovement Script
    Added gravity and a jump button

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // ================= Varibles ==================
+    [Header("Components")]
     [SerializeField] private CharacterController controller;
+    [SerializeField] private Transform groundCheck;
+
+    [Header("LayerMasks")]
+    [SerializeField] private LayerMask groundMask;
+
+    [Header("RayValues")]
+    [SerializeField] private float groundDistance = 0.4f;
+
+    [Header("MovementValues")]
     [SerializeField] private float speed = 12f;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float jumpHeight = 3f;
+
+    // ================== private ==================
+
+    private Vector3 velocity;
+
+    // =============================================
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +35,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movePlayer();
+        MovePlayer();
     }
 
-    private void movePlayer()
+    private void MovePlayer()
     {
         // Setting up controls
         float x = Input.GetAxis("Horizontal");
@@ -29,5 +48,42 @@ public class PlayerMovement : MonoBehaviour
 
         // Moves Player
         controller.Move(move * speed * Time.deltaTime);
+
+        // Gravity
+        PlayerGravity();
+
+        // JumpControls
+        PlayerJump();
+    }
+
+    private void PlayerGravity()
+    {
+        // Stops raising gravity when player is on ground
+        if (IsGrounded() && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // Adds gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private bool IsGrounded()
+    {
+        // Checks if player is grounded
+        if (Physics.CheckSphere(groundCheck.position, groundDistance, groundMask))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void PlayerJump()
+    {
+        if(Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
     }
 }
