1844f817582466aec5ba21a31d2fe2d187b40d0e
Tobias Gagnon <Tobias11211@gmail.com>
2022-06-22 01:20:54 -0300

    [ADDED] SprintButton
    Shift

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,10 +6,9 @@ public class CharacterMovement : MonoBehaviour
 {
 
     [Header("Varibles")]
-    public float speed = 12f;
-    public float gravity = -9.81f;
     public float jumpHeight = 3f;
-
+    public float speed = 6f;
+    public float sprintSpeed = 9f;
     [Header("GroundCheck")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundDistence;
@@ -18,11 +17,13 @@ public class CharacterMovement : MonoBehaviour
     public CharacterController cController;
 
     private Vector3 velocity;
+    private float gravity = -9.81f;
+    private float _speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _speed = speed;
     }
 
     // Update is called once per frame
@@ -30,6 +31,7 @@ public class CharacterMovement : MonoBehaviour
     {
         PlayerMove();
         PlayerGravity();
+        PlayerSprint();
         PlayerJump();
         PlayerGrounded();
 
@@ -42,7 +44,7 @@ public class CharacterMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        cController.Move(move * speed * Time.deltaTime);
+        cController.Move(move * _speed * Time.deltaTime);
     }
 
     private void PlayerGravity()
@@ -63,11 +65,18 @@ public class CharacterMovement : MonoBehaviour
 
     private void PlayerGrounded()
     {
-        if (IsGrounded() && velocity.y < 0) velocity.y = -2f;
+        if(IsGrounded() && velocity.y < 0) velocity.y = -2f;
     }
 
     private void PlayerJump()
     {
         if(Input.GetButtonDown("Jump") && IsGrounded()) velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
+
+    private void PlayerSprint()
+    {
+        //sprint
+        if (Input.GetButton("Sprint") && IsGrounded()) _speed = sprintSpeed;
+        else if(IsGrounded()) _speed = speed;
     }
 }
